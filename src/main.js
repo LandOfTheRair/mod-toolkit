9995e02956ce9f979602d839107415a0f33fee94
Kyle Kemp <kyle@seiyria.com>
2021-06-10 10:05:55 -0500

    adjust todos

--- src/main.js
+++ src/main.js
@@ -7,16 +7,13 @@
 // TODO: with map, check lair names against monsters
 // TODO: with map, check npc names / dialogs
 // TODO: audit tool that prints abnormal settings (item.destroyOnDrop, item.requirements, stats.actionSpeed, etc)
-// TODO: get a list of all traits via HTTP - should be hosted on play.rair.land when ready
-// TODO: get all tradeskills, skills, traits, and other external data via an api or something?
 // TODO: npc sprite should be an array
-// TODO: host Content json files somewhere (content.rair.land?) and have an import option in each subtype to import stuff from there
-// TODO: provide link to template map and allow for easy download/setup for Tiled - provide sample config with spritesheets in correct location
 // TODO: make sure all items and npcs have unique names
 // TODO: when updating item name, update npc gear as well
 // TODO: when updating npc id, update spawner npcId list
 // TODO: remove size from selector components; put size on the implementer
 // TODO: support tooltips for selector components
+// TODO: add export mod support to export to json, with compiled (and cleaned) result as well as original mod. lots of extra shit needs to be cleaned from the export due to how the editor works
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue.min.css';
